cbc224f6ff45ab149c8a198b13611ebb1b15614e
SISU <atgsisu@gmail.com>
2022-08-20 14:18:06 +0900

    Create TodoList class
    TodoList State & State Management class
    at ST class, there are four method
    add todo to list (addTodo)
    edit todo already exist(editTodo),
    toggle todo's completed argument(toggleTodo),
    remove todo element on the list(removeTodo)

---
+++ lib/providers/todo_list.dart
@@ -0,0 +1,78 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+
+import '../models/todo_model.dart';
+
+class TodoListState extends Equatable {
+  final List<Todo> todos;
+  TodoListState({
+    required this.todos,
+  });
+  factory TodoListState.initial() {
+    return TodoListState(todos: [
+      Todo(id: '1', desc: 'Clean my room'),
+      Todo(id: '2', desc: 'Wash the dish'),
+      Todo(id: '3', desc: 'Review TOEIC RC TEST 2,4'),
+    ]);
+  }
+
+  @override
+  List<Object> get props => [todos];
+
+  @override
+  bool get stringify => true;
+
+  TodoListState copyWith({
+    List<Todo>? todos,
+  }) {
+    return TodoListState(
+      todos: todos ?? this.todos,
+    );
+  }
+}
+
+class TodoList with ChangeNotifier {
+  TodoListState _state = TodoListState.initial();
+  TodoListState get state => _state;
+
+  void addTodo(String newDesc) {
+    // _state.todos.add(Todo(desc: newDesc));
+    // notifyListeners();
+    final Todo newTodo = Todo(desc: newDesc);
+    final newTodos = [..._state.todos, newTodo];
+
+    _state = _state.copyWith(todos: newTodos);
+    notifyListeners();
+  }
+
+  void editTodo(String id, String todoDesc) {
+    final newTodos = _state.todos.map((Todo todo) {
+      if (id == todo.id) {
+        return Todo(id: id, desc: todoDesc, completed: todo.completed);
+      }
+      return todo;
+    }).toList();
+
+    _state = _state.copyWith(todos: newTodos);
+    notifyListeners();
+  }
+
+  void toggleTodo(String id) {
+    final newTodos = _state.todos.map((Todo todo) {
+      if (todo.id == id) {
+        return Todo(id: todo.id, desc: todo.desc, completed: !todo.completed);
+      }
+      return todo;
+    }).toList();
+
+    _state = _state.copyWith(todos: newTodos);
+    notifyListeners();
+  }
+
+  void removeTodo(Todo todo) {
+    final newTodo = _state.todos.where((Todo t) => t.id != todo.id).toList();
+    _state = _state.copyWith(todos: newTodo);
+    notifyListeners();
+  }
+}
