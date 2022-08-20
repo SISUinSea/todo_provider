1d945eb4f50df9384d61e2bddaf9282396d79846
SISU <atgsisu@gmail.com>
2022-08-20 15:20:27 +0900

    Add Provider on MaterialApp widget
    Use  ChangeNotifierProvider to provide independent state, and use ChangeNotiferProxyProviderN to provider computed state

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_provider/providers/providers.dart';
 
 import './pages/todo_page.dart';
 
@@ -9,11 +11,34 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TODOS',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const TodosPage(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<TodoFilter>(create: (context) => TodoFilter()),
+        ChangeNotifierProvider<TodoSearch>(create: (context) => TodoSearch()),
+        ChangeNotifierProvider<TodoList>(create: (context) => TodoList()),
+        ChangeNotifierProxyProvider<TodoList, ActiveTodoCount>(
+          create: (context) => ActiveTodoCount(),
+          update: (BuildContext context, TodoList todoList,
+                  ActiveTodoCount? activeTodoCount) =>
+              activeTodoCount!..update(todoList),
+        ),
+        ChangeNotifierProxyProvider3<TodoFilter, TodoSearch, TodoList,
+            FilteredTodos>(
+          create: (context) => FilteredTodos(),
+          update: (BuildContext context,
+                  TodoFilter todoFilter,
+                  TodoSearch todoSearch,
+                  TodoList todoList,
+                  FilteredTodos? filteredTodos) =>
+              filteredTodos!..update(todoList, todoSearch, todoFilter),
+        )
+      ],
+      child: MaterialApp(
+        title: 'TODOS',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: const TodosPage(),
+      ),
     );
   }
 }
