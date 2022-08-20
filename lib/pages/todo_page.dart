9174aaf73cca6ce2fcbd61a9b9c4fbf9ac80d65d
SISU <atgsisu@gmail.com>
2022-08-20 15:48:01 +0900

    UI Start! : Create TODOS header
    widget coined TodoHeader
    
    add print method in active todo count class's method, coined update to see how added with constructory things work

--- lib/pages/todo_page.dart
+++ lib/pages/todo_page.dart
@@ -1,14 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/providers.dart';
 
 class TodosPage extends StatelessWidget {
   const TodosPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('TODOS'),
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 40.0),
+              child: Column(
+                children: [
+                  TodoHeader(),
+                ],
+              )),
+        ),
       ),
+    );
+  }
+}
+
+class TodoHeader extends StatelessWidget {
+  const TodoHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          'TODOS',
+          style: TextStyle(fontSize: 40.0),
+        ),
+        Text(
+          '${context.watch<ActiveTodoCount>().state.activeTodoCount} items left',
+          style: TextStyle(color: Colors.redAccent, fontSize: 20.0),
+        )
+      ],
     );
   }
 }
